b0bdb2b534767830b548981cdfcbb29936a8b89e
Jeff Amfahr <amfahrj@seapine.com>
2012-04-12 15:54:46 -0400

    Media

--- Raceday/Raceday/SettingsViewController.m
+++ Raceday/Raceday/SettingsViewController.m
@@ -7,25 +7,29 @@
 //
 
 #import "SettingsViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
 
-@interface SettingsViewController ()
+@interface SettingsViewController () <MPMediaPickerControllerDelegate>
+@property (strong) MPMusicPlayerController *musicPlayer;
 
 @end
 
 @implementation SettingsViewController
 @synthesize playlistLabel=_playlistLabel;
+@synthesize musicPlayer=_musicPlayer;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.musicPlayer = [MPMusicPlayerController iPodMusicPlayer];
+    [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
 }
 
 - (void)viewDidUnload
 {
     [self setPlaylistLabel:nil];
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
+    [[UIApplication sharedApplication] endReceivingRemoteControlEvents];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -37,5 +41,56 @@
 }
 
 - (IBAction)browsePlaylist:(id)sender {
+    MPMediaPickerController *mediaPicker = [[MPMediaPickerController alloc] initWithMediaTypes:MPMediaTypeMusic];
+    
+    mediaPicker.delegate = self;
+    mediaPicker.prompt = @"Choose your song:";
+    mediaPicker.navigationController.toolbar.barStyle = UIBarStyleBlackOpaque;
+    mediaPicker.allowsPickingMultipleItems = YES;
+    
+    [self presentModalViewController:mediaPicker animated:YES];
 }
+
+- (void) mediaPicker: (MPMediaPickerController *) mediaPicker didPickMediaItems: (MPMediaItemCollection *) mediaItemCollection
+{
+    if (mediaItemCollection) {
+        
+        [self.musicPlayer setQueueWithItemCollection: mediaItemCollection];
+        [self.musicPlayer play];
+    }
+    
+	[self dismissModalViewControllerAnimated: YES];
+}
+
+
+- (void) mediaPickerDidCancel: (MPMediaPickerController *) mediaPicker 
+{
+	[self dismissModalViewControllerAnimated: YES];
+}
+
+- (void) remoteControlReceivedWithEvent: (UIEvent *) receivedEvent {
+    
+    if (receivedEvent.type == UIEventTypeRemoteControl) {
+        
+        switch (receivedEvent.subtype) {
+                
+            case UIEventSubtypeRemoteControlTogglePlayPause:
+                //[self playOrStop: nil];
+                break;
+                
+            case UIEventSubtypeRemoteControlPreviousTrack:
+                //[self previousTrack: nil];
+                break;
+                
+            case UIEventSubtypeRemoteControlNextTrack:
+                //[self nextTrack: nil];
+                break;
+                
+            default:
+                break;
+        }
+    }
+}
+
+
 @end
